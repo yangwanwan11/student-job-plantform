50a130b218b59c085cc855d18eac494294066825
yyyyy <17525886372@qq.com>
2026-04-22 08:49:45 +0800

    提交学生求职平台war项目

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,28 @@
+
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(DemoApplication.class);
+    }
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "war包 CI/CD 部署成功";
+    }
+}
